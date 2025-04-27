9621227c58d1700d153a46b9bfc906275d6aa806
semihataman161 <semihataman16@gmail.com>
2025-04-27 22:09:36 +0300

    Wrap web accessible resource into a function to fix name collision bugs

--- src/web.accessible.resource.ts
+++ src/web.accessible.resource.ts
@@ -7,57 +7,61 @@ import { clickFollowers, clickFollowing } from "./utils/user.utils";
 import FetchInterceptor from "./services/FetchInterceptor";
 import { FetchRoute } from "./services/FetchInterceptor/index.type";
 
-let urlObserver: UrlObserver;
-let fetchInterceptor: FetchInterceptor;
+function initializeScript() {
+  let urlObserver: UrlObserver;
+  let fetchInterceptor: FetchInterceptor;
 
-// https://www.instagram.com/api/v1/friendships/1389322411/followers/?count=12&search_surface=follow_list_page
+  // https://www.instagram.com/api/v1/friendships/1389322411/followers/?count=12&search_surface=follow_list_page
 
-const handleClickFollowers = (userName: string) => {
-  const userId = userIdNameMap.getKeyByValue(userName);
+  const handleClickFollowers = (userName: string) => {
+    const userId = userIdNameMap.getKeyByValue(userName);
 
-  const route: FetchRoute = {
-    url: `${apiUrlV1}/friendships/${userId}/followers`,
-    method: "GET",
-    beforeExecute: async (url, request) => {
-      console.log("url: ", url);
-      console.log("request: ", request);
-      return false;
-    },
+    const route: FetchRoute = {
+      url: `${apiUrlV1}/friendships/${userId}/followers`,
+      method: "GET",
+      beforeExecute: async (url, request) => {
+        console.log("url: ", url);
+        console.log("request: ", request);
+        return false;
+      },
+    };
+    fetchInterceptor.addRoute(route);
+
+    clickFollowers(userName);
   };
-  fetchInterceptor.addRoute(route);
 
-  clickFollowers(userName);
-};
+  const handleClickFollowing = (userName: string) => {
+    // const route: FetchRoute = {};
+    // fetchInterceptor.addRoute(route);
 
-const handleClickFollowing = (userName: string) => {
-  // const route: FetchRoute = {};
-  // fetchInterceptor.addRoute(route);
+    clickFollowing(userName);
+  };
 
-  clickFollowing(userName);
-};
+  const onPageRefresh = (path: string) => {
+    const firstSegment = getFirstPathSegment(path);
+    const shouldObserve = userIdNameMap.hasValue(firstSegment);
 
-const onPageRefresh = (path: string) => {
-  const firstSegment = getFirstPathSegment(path);
-  const shouldObserve = userIdNameMap.hasValue(firstSegment);
+    if (!shouldObserve) {
+      return;
+    }
 
-  if (!shouldObserve) {
-    return;
-  }
+    handleClickFollowers(firstSegment);
+    // handleClickFollowing(firstSegment);
+  };
 
-  handleClickFollowers(firstSegment);
-  // handleClickFollowing(firstSegment);
-};
+  const onPathChange = (path: string) => {
+    console.log("ChangedPath: ", path);
+  };
 
-const onPathChange = (path: string) => {
-  console.log("ChangedPath: ", path);
-};
+  const startScript = () => {
+    fetchInterceptor = new FetchInterceptor();
+    fetchInterceptor.start();
 
-const startApp = () => {
-  fetchInterceptor = new FetchInterceptor();
-  fetchInterceptor.start();
+    urlObserver = new UrlObserver(hostUrl, (path) => onPathChange(path));
+    onPageRefresh(urlObserver.path);
+  };
 
-  urlObserver = new UrlObserver(hostUrl, (path) => onPathChange(path));
-  onPageRefresh(urlObserver.path);
-};
+  startScript();
+}
 
-startApp();
+initializeScript();
